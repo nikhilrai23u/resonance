11ee0e98a0131a118af9d01fbe684846c7e526e2
nikhil rai <nikhil.rai23u@gmail.com>
2026-07-15 15:54:13 +0530

    06: tts generation & audio player

--- src/features/text-to-speech/components/generate-button.tsx
+++ src/features/text-to-speech/components/generate-button.tsx
@@ -1,36 +1,36 @@
-"use client" ;
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 
-export function GenerateButton ({
-    size , 
-    disabled ,
-    isSubmitting , 
-    onSubmit , 
-    className , 
-} : {
-    size?: "default" | "sm" ; 
-    disabled : boolean ; 
-    isSubmitting : boolean ;  
-    onSubmit : () => void ;  
-    className?:  string ; 
+export function GenerateButton({
+  size,
+  disabled,
+  isSubmitting,
+  onSubmit,
+  className,
+}: {
+  size?: "default" | "sm";
+  disabled: boolean;
+  isSubmitting: boolean;
+  onSubmit: () => void;
+  className?: string;
 }) {
-    return (
-        <Button
-            size={size}
-            className={className}
-            onSubmit={onSubmit}
-            disabled={disabled}
-        >
-            {isSubmitting ? (
-                <>
-                    <Spinner className="size-3" />
-                    Generating...
-                </>
-            ) : (
-                "Generate Speech"
-            )}
-        </Button>
-    )
-} ; 
+  return (
+    <Button
+      size={size}
+      className={className}
+      onClick={onSubmit}
+      disabled={disabled}
+    >
+      {isSubmitting ? (
+        <>
+          <Spinner className="size-3" />
+          Generating...
+        </>
+      ): (
+        "Generate speech"
+      )}
+    </Button>
+  );
+};
